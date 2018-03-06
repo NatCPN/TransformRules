d0c5a9f4d2b03579b09769b1e63f71fedf60c72b
NatCPN <bcfs@cin.ufpe.br>
2018-03-06 01:52:54 -0300

    Add files via upload

---
+++ EmpiricalAnalyses/PC/Mutants/PC62.java
@@ -0,0 +1,49 @@
+// This is mutant program. 
+// Author : ysma 
+
+public class PC62
+{
+
+    public int the_command_in_control_output = 3;
+
+    public boolean old_the_left_priority_button = false;
+
+    public boolean old_the_right_priority_button = false;
+
+    public java.lang.Integer run( boolean the_left_priority_button, boolean the_right_command, boolean the_left_command, boolean the_right_priority_button )
+    {
+        if (!(the_right_command == true && the_left_command == true && !(the_right_priority_button == true)) && !(the_left_priority_button == true)) {
+            the_command_in_control_output = 0;
+        } else {
+            if (the_right_command == true && !(the_left_command == true) && !(the_right_priority_button == true) && !(the_left_priority_button == true)) {
+                the_command_in_control_output = 1;
+            } else {
+                if (!(the_right_command == true) && the_left_command == true && !(the_right_priority_button == true) && !(the_left_priority_button == true)) {
+                    the_command_in_control_output = 2;
+                } else {
+                    if (!(the_right_command == true) && !(the_left_command == true) && !(the_right_priority_button == true) && !(the_left_priority_button == true)) {
+                        the_command_in_control_output = 3;
+                    } else {
+                        if (!(the_right_priority_button == true) && the_left_priority_button == true) {
+                            the_command_in_control_output = 4;
+                        } else {
+                            if (the_right_priority_button == true && !(the_left_priority_button == true)) {
+                                the_command_in_control_output = 5;
+                            } else {
+                                if (the_right_priority_button == true && old_the_right_priority_button == false && old_the_left_priority_button == true && the_left_priority_button == true) {
+                                    the_command_in_control_output = 4;
+                                } else {
+                                    if (the_left_priority_button == true && old_the_right_priority_button == true && old_the_left_priority_button == false && the_right_priority_button == true) {
+                                        the_command_in_control_output = 5;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return the_command_in_control_output;
+    }
+
+}
